1ff41fdb1567527de3e9c1a0a44b7fa9b8914bc9
rjmacarthy <richardmacarthy@protonmail.com>
2022-09-29 14:53:20 +0100

    fix tests

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -2,7 +2,7 @@ import { inverseTransform, transform } from '../index'
 import { matrix, output, matrixInversed } from './fixtures'
 
 test('transform', () => {
-  expect(transform(matrix)).toStrictEqual(output)
+  expect(transform(matrix, ['a', 'b', 'c', 'd', 'e'])).toStrictEqual(output)
 })
 
 test('inverseTransform', () => {
